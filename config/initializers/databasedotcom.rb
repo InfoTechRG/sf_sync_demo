34acfd774aec792882b5df0719e0da671ae34a67
Liam Nediger <lnediger@gmail.com>
2013-04-24 17:08:34 -0400

    Updating to work with version 1.1.1 of the gem.

--- config/initializers/databasedotcom.rb
+++ config/initializers/databasedotcom.rb
@@ -4,5 +4,7 @@ sfdc_auth_config = YAML.load_file("#{::Rails.root.to_s}/config/databasedotcom.ym
   
 $sf_client = Databasedotcom::Client.new("config/databasedotcom.yml")
 $sf_client.authenticate :username => sfdc_auth_config['username'], :password => sfdc_auth_config['password']
-$sf_client.sobject_module = Databasedotcom
-$sf_client.materialize "Account"
+
+module SalesforceArSync::SalesforceSync
+  SF_CLIENT = $sf_client
+end
