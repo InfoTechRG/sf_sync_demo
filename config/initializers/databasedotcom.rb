c17743dcf1827a14e6d22ed637167574d8ae573c
Liam Nediger <lnediger@gmail.com>
2013-04-25 10:13:27 -0400

    Let databasedotcom use environment variables for config

--- config/initializers/databasedotcom.rb
+++ config/initializers/databasedotcom.rb
@@ -1,9 +1,13 @@
 require "databasedotcom"
 
-sfdc_auth_config = YAML.load_file("#{::Rails.root.to_s}/config/databasedotcom.yml")
-  
-$sf_client = Databasedotcom::Client.new("config/databasedotcom.yml")
-$sf_client.authenticate :username => sfdc_auth_config['username'], :password => sfdc_auth_config['password']
+if File.exist?("#{::Rails.root.to_s}/config/databasedotcom.yml")
+  sfdc_auth_config = YAML.load_file("#{::Rails.root.to_s}/config/databasedotcom.yml")
+  $sf_client = Databasedotcom::Client.new("config/databasedotcom.yml")
+  $sf_client.authenticate :username => sfdc_auth_config['username'], :password => sfdc_auth_config['password']
+else
+  $sf_client = Databasedotcom::Client.new
+  $sf_client.authenticate :username => ENV['DATABASEDOTCOM_USERNAME'], :password => ENV['DATABASEDOTCOM_PASSWORD']
+end
 
 module SalesforceArSync::SalesforceSync
   SF_CLIENT = $sf_client
